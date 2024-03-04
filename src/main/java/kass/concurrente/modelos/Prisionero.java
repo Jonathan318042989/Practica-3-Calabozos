0d4d4515fbe04c21bc6dce09da38a0cf0d0a46f9
Jonathan318 <jonathanmartinezc0910@ciencias.unam.mx>
2024-03-04 00:14:17 -0600

    Acompleto Prisionero y Vocero(?), de este último falta pero no se que es xd

--- src/main/java/kass/concurrente/modelos/Prisionero.java
+++ src/main/java/kass/concurrente/modelos/Prisionero.java
@@ -2,6 +2,7 @@ package kass.concurrente.modelos;
 
 /**
  * Clase que modela un prisioner
+ * 
  * @version 1.0
  * @author <Su equipo>
  */
@@ -12,13 +13,69 @@ public class Prisionero {
 
     /**
      * Metodo constructor para generar un prisionero
-     * @param id El identificador del prisionero
+     * 
+     * @param id       El identificador del prisionero
      * @param esVocero true si es Vocero false en otro caso
-     * @param marcado true si ya paso
+     * @param marcado  true si ya paso
      */
-    public Prisionero(Integer id, Boolean esVocero, Boolean marcado){
-        //Aqui pon lo que falta
+    public Prisionero(Integer id, Boolean esVocero, Boolean marcado) {
+        this.setId(id);
+        this.setEsVocero(esVocero);
+        this.setMarcado(marcado);
     }
 
-    //Annadir getter and setter, toString.
+    /**
+     * Devuelve el Id del prisionero
+     * 
+     * @return id del prisionero
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * Establece el id de este prisionero
+     * 
+     * @param id Id del prisionero
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * Indica si el prisionero es vocero
+     * 
+     * @return true si es vocero, false si no lo es
+     */
+    public Boolean getEsVocero() {
+        return esVocero;
+    }
+
+    /**
+     * Establece si el prisionero es vocero o no
+     * 
+     * @param esVocero booleano para indicar si lo es o no
+     */
+    public void setEsVocero(Boolean esVocero) {
+        this.esVocero = esVocero;
+    }
+
+    /**
+     * Indica si el prisionero ya pasó o no
+     * 
+     * @return true si ya pasó, false si no
+     */
+    public Boolean getMarcado() {
+        return marcado;
+    }
+
+    /**
+     * Establece si el prisionero ya pasó
+     * 
+     * @param marcado booleano para indicar si pasó o no
+     */
+    public void setMarcado(Boolean marcado) {
+        this.marcado = marcado;
+    }
+
 }
--- src/main/java/kass/concurrente/modelos/Vocero.java
+++ src/main/java/kass/concurrente/modelos/Vocero.java
@@ -1,17 +1,44 @@
 package kass.concurrente.modelos;
 
 /**
- * Este ess quien lleva la cuenta de los prisioneros que han entrado a la habitacion
+ * Este ess quien lleva la cuenta de los prisioneros que han entrado a la
+ * habitacion
  * a parte de los atributos de Prisionero, tambien posee un contador
+ * 
  * @author <Su equipo>
  * @version 1.0
  */
-public class Vocero extends Prisionero{
+public class Vocero extends Prisionero {
     protected Integer contador;
+
+    /**
+     * Constructor para el vocero
+     * 
+     * @param id       Id de prisionero
+     * @param esVocero bool que indica si es vocero (debería ser true)
+     * @param marcado  bool que indica si ya pasó
+     */
     public Vocero(Integer id, Boolean esVocero, Boolean marcado) {
         super(id, esVocero, marcado);
-        //Completar y hacer documentacion
+        // Completar y hacer documentacion
     }
-    
-    //Mismo caso que el otro, annadir getter and setter
+
+    /**
+     * Regresa el contador del vocero, cuantos prisioneros han pasado
+     * 
+     * @return El contador del vocero
+     */
+    public Integer getContador() {
+        return contador;
+    }
+
+    /**
+     * Establece el contador del vocero, cuantos prisioneros han pasado
+     * 
+     * @param contador Cuantos prisioneros han pasado
+     */
+    public void setContador(Integer contador) {
+        this.contador = contador;
+    }
+
 }
